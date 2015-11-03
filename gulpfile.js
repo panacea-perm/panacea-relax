22f4d84917550020f141c7948845781af30a4f6e
Artem Markov <drownbes@gmail.com>
2015-11-03 14:24:20 +0500

    Deploy to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,23 @@ var replace = require('gulp-replace');
 var flatten = require('gulp-flatten');
 var cssmin = require('gulp-cssmin');
 var es = require('event-stream');
-
+var ghPages = require('gulp-gh-pages');
 
 var jsBundle = gulp.src(
     mainBowerFiles({filter: /\.js$/})
     .concat(['src/js/main.js'])
 )
 .pipe(concat('bundle.js'))
-//.pipe(uglify())
-.pipe(gulp.dest('./dist'));
+.pipe(uglify())
+.pipe(gulp.dest('./dist/js'));
 
 var cssBundle = gulp.src(
     mainBowerFiles({filter: /\.css$/})
     .concat(['src/styles/main.css'])
 )
 .pipe(concat('bundle.css'))
-//.pipe(cssmin())
-.pipe(gulp.dest('./dist'));
+.pipe(cssmin())
+.pipe(gulp.dest('./dist/css'));
 
 
 gulp.task('copy' , function() {
@@ -38,6 +38,11 @@ gulp.task('build', ['copy'], function () {
     .pipe(inject(es.merge(jsBundle, cssBundle), { read:false }))
     .pipe(replace('\/dist', '.'))
     .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
 
 
